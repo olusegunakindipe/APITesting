04bb8c7b83a57b0a3b053898d62a237d059fd5b2
aisolae <aremu_abisoye@live.com>
2020-02-13 11:24:11 +0800

    Check headers after getting the ApI

--- tests/api/_17_User_GetOrder_Info_Cest.php
+++ tests/api/_17_User_GetOrder_Info_Cest.php
@@ -15,9 +15,9 @@ class _17_User_GetOrder_Info_Cest
 
         $I->AdminLogin();
         $I->wantTo('Get the Matching fields in the List');
+        $data = $I->sendGET('User/GetOrderInfo/2019-02-07/2020-02-07');
         $I->haveHttpHeader('accept', 'application/json');
         $I->seeHttpHeader('Content-Type','application/json');
-        $data = $I->sendGET('User/GetOrderInfo/2019-02-07/2020-02-07');
 
         $I->dontSeeResponseContainsJson(['code' => 401]);
         // it can match tree-like structures as well
@@ -26,8 +26,7 @@ class _17_User_GetOrder_Info_Cest
    
          $I->grabResponse();
          $I->DisplayResponse($data);
-        //  $I->CheckResponseTimeEquals();
-        // $I->CheckId();
+
         $I->seeResponseIsJson();
         $I->seeResponseCodeIs(200); 
     }
