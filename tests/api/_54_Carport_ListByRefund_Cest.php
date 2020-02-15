4d323ad18dd9c1a96aa329e9e2d0333f2795dfb3
aisolae <aremu_abisoye@live.com>
2020-02-16 05:57:14 +0800

    Split api path, Check Json data type

--- tests/api/_54_Carport_ListByRefund_Cest.php
+++ tests/api/_54_Carport_ListByRefund_Cest.php
@@ -15,21 +15,25 @@ class _54_Carport_ListByRefund_Cest
     {
         $I->AdminLogin();
         $I->wantTo('Check if the some data are present in the API');
-        $data = $I->sendGET('/Carport/ListByRefund/2019-02-11/2020-02-11/1/20');
+        $startDate = '2020-01-11';
+        $endDate = '2020-02-11';
+        $page = 1;
+        $size = 20;
+        $urlParams = [
+            $startDate,
+            $endDate,
+            $page,
+            $size
+        ];
+        $api = "/Carport/ListByRefund/";
+        $path = $api . join("/", $urlParams);
+        $data = $I->sendGET($path);
         $I->wantTo('Get response Time for this Api');
         $I->DisplayResponse($data);
         $I->CheckAccount($data);
-        $I->seeResponseContainsJson([
-            'ID' => 224,
-            'TYPE'=> 1,
-            'REFUND_ORDER_NO'=>'RF20191230091257319',
-            'USER_ID' => '100000094025',
-            'CELL_ID' => '1304290002',
-            'CREATE_TIME' => '2019-12-30 09:55:57.380',
-            'ORDER_NO' => 'OSXHS2019121119260253286574',
-        ]);
-        $I->SeeResponseContainsJson(['code' => 200]);
         $I->seeResponseIsJson(); 
-        $I->seeResponseCodeIs(\Codeception\Util\HttpCode::OK); 
+        $I->seeResponseCodeIs(\Codeception\Util\HttpCode::OK);
+        $I->seeResponseCodeIs(200);
+ 
     }
 }
