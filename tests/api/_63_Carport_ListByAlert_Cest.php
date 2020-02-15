8953e383a49b571f9ea010bd7e60bd7d082ae9ab
aisolae <aremu_abisoye@live.com>
2020-02-16 06:49:43 +0800

    Splitted data, Checked Json data

--- tests/api/_63_Carport_ListByAlert_Cest.php
+++ tests/api/_63_Carport_ListByAlert_Cest.php
@@ -14,20 +14,23 @@ class _63_Carport_ListByAlert_Cest
     {
         $I->AdminLogin();
         $I->wantTo('Check if the some data are present in the API');
-        $data = $I->sendGET('/Carport/ListByAlert/2019-02-11/2020-02-11/1/20');
+        $startDate = '2020-01-11';
+        $endDate = '2020-02-11';
+        $page = 1;
+        $size = 20;
+        $urlParams = [
+            $startDate,
+            $endDate,
+            $page,
+            $size
+        ];
+        $api = "/Carport/ListByAlert/";
+        $path = $api . join("/", $urlParams);
+        $data = $I->sendGET($path);
         $I->wantTo('Get response Time for this Api');
         $I->DisplayResponse($data);
-        $I->wantTo('check if the data is not empty');
-        $I->seeResponseContainsJson(['total' => 92, 'pageTotal'=>5, 'size'=>20]);
-        $I->seeResponseContainsJson([
-            'DEVICE_TYPE' => 1,
-            'DEVICE_SN'=> '862980043422211',
-            'CELL_ID'=>'3501040005',
-            'CARPORT_ID' => '410305000420190509135908',
-            'CARPORT_NAME' => '易安充小区测试车棚',
-            'CELL_NAME' => '易安充测试小区',
-            'ALERT_STATUS' => 0
-        ]);
+        $I->wantTo('check Carport alert data');
+        $I->CheckCarportAlertData($data);
         $I->seeResponseIsJson();
         $I->seeResponseCodeIs(200); 
     }
