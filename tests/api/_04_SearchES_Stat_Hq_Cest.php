c0499d2b2ccafa8730d98ee31b75788c08a3f0e3
aisolae <aremu_abisoye@live.com>
2020-02-15 22:35:35 +0800

    Splitted API path and set the date to a month interval

--- tests/api/_04_SearchES_Stat_Hq_Cest.php
+++ tests/api/_04_SearchES_Stat_Hq_Cest.php
@@ -17,7 +17,15 @@ class _04_SearchES_Stat_Hq_Cest
         $I->wantTo('Get the ID and Matching fields in SearchES');
         $I->haveHttpHeader('accept', 'application/json');
         $I->haveHttpHeader('Content-Type','application/json');
-        $data = $I->sendGET('SearchES/Stat/hq/top/2020-01-07/2020-02-05');
+        $startDate = '2020-01-07';
+        $endDate = '2020-02-05';
+        $urlParams = [
+            $startDate,
+            $endDate
+        ];
+        $api = "/SearchES/Stat/hq/top/";
+        $path = $api . join("/", $urlParams);
+        $data = $I->sendGET($path);
         $I->CheckVariousData($data);
         $I->DisplayResponse($data); //This is response Time
         // $data = $I->grabDataFromResponseByJsonPath('debug');
