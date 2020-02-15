9a46c8971d3b6408bfe96392858f80a825f3159f
aisolae <aremu_abisoye@live.com>
2020-02-16 02:42:12 +0800

    Check data correctness

--- tests/api/_28_Meter_SearchByAll_Cest.php
+++ tests/api/_28_Meter_SearchByAll_Cest.php
@@ -16,11 +16,10 @@ class _28_Meter_SearchByAll_Cest
         $I->AdminLogin();
         $I->wantTo('Check if the data are correctly stored');
         $data = $I->sendGET('Meter/SearchByAll');
-        //  $I->grabResponse();
         $I->DisplayResponse($data);
         $I->wantTo('Check if the specific elctricity number is included in the list');
         $I->CheckForElectricityNo($data);
-        // $I->CheckId();
+        $I->CheckResponseTimeEquals($data);
         $I->seeResponseIsJson();
         $I->seeResponseCodeIs(200); 
     }
