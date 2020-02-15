97a01aac6cfbb90fbfe8d145a5cf95b00aea93dd
aisolae <aremu_abisoye@live.com>
2020-02-16 01:39:34 +0800

    Splitted data and checked user balance data

--- tests/api/_18_User_Balance_LogCest.php
+++ tests/api/_18_User_Balance_LogCest.php
@@ -11,20 +11,31 @@ class _18_User_Balance_LogCest
     {
     }
 
-    public function UserManagerList(ApiTester $I)
+    public function UserBalanceLog(ApiTester $I)
     {
         $I->AdminLogin();
-        $I->wantTo('Get the response Time of ToolMenuApi');
-        $I->sendGET('User/BalanceLog/2019-02-07/2020-02-07/1/20');
+        $I->wantTo('Get the data in this API');
+        $startDate= "2020-01-07";
+        $endDate = "2020-02-07";
+        $page = 1;
+        $size = 20;
+        $urlParams = [
+            $startDate,
+            $endDate,
+            $page,
+            $size
+        ];
+        $api = "/User/BalanceLog/";
+        $path = $api . join("/", $urlParams);
+        $data = $I->sendGET($path);
         $I->wantTo('PrintOut the response and peak Time');
         $data = $I->grabDataFromResponseByJsonPath('debug');
-        echo "Response Time is:";
+        echo "Response Time is:"; //Response Time
         print_r($data[0]['time']);
-        echo "Peak Time:"; //This is response Time
+        echo "Peak Time:"; 
         print_r($data[0]['peak']); //This is the peak time
         $I->CheckResponseTimeEquals($data);
         $I->CheckUsereBalanceData($data);
-        $I->seeResponseContainsJson(['total' => 652206, 'pageTotal' => 32611]);
         $I->CheckResponseTimeEquals($data);
         $I->seeResponseIsJson();
         $I->seeResponseCodeIs(200); 
