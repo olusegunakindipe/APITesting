710cdb5a62f6d8d542fd8adb5feca38c1530faa5
aisolae <aremu_abisoye@live.com>
2020-02-16 02:59:22 +0800

    Splitted api path, Check Alert data

--- tests/api/_30_Alert_ListBy_Cest.php
+++ tests/api/_30_Alert_ListBy_Cest.php
@@ -14,13 +14,25 @@ class _30_Alert_ListBy_Cest
     public function AlertListBy(ApiTester $I) 
     {
         $I->AdminLogin();
-        $I->wantTo('Get the response Time of ToolMenuApi');
-        $data= $I->sendGET('Alert/ListBy/2019-02-07/2020-02-07/1/20');
+        $I->wantTo('Get the data Information');
+        $startDate ="2020-01-07";
+        $endDate = "2020-02-07";
+        $page = 1;
+        $size = 20;
+        $urlParams = [
+            $startDate,
+            $endDate,
+            $page,
+            $size
+        ];
+        $api = "/Alert/ListBy/";
+        $path = $api . join("/", $urlParams);
+        $data = $I->sendGET($path);
         $I->wantTo('check Info about the response and peak Time');
         $I->DisplayResponse($data);
         $I->AlertTime($data);
-        $I->dontSeeResponseContainsJson(['total' => 0]);
         $I->seeResponseIsJson();
+        $I->dontSeeResponseCodeIs(401);
         $I->seeResponseCodeIs(\Codeception\Util\HttpCode::OK); 
     }
 }
