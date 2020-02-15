26be7fbf7f4ca63f331342ad6f23ce050625a7d6
aisolae <aremu_abisoye@live.com>
2020-02-16 07:56:31 +0800

    Splitted data, Checked Json data

--- tests/api/_71_Project_ListBy_Cest.php
+++ tests/api/_71_Project_ListBy_Cest.php
@@ -15,13 +15,20 @@ class _71_Project_ListBy_Cest
     {
         $I->AdminLogin();
         $I->wantTo('Check if the some data are present in the API');
-        $data = $I->sendGET('/Project/ListBy/1/20');
+        $page = 1;
+        $size = 20;
+        $urlParams = [
+            $page,
+            $size
+        ];
+        $api = "/Project/ListBy/";
+        $path = $api . join("/", $urlParams);
+        $data = $I->sendGET($path);
         $I->wantTo('Get response Time for this Api');
         $I->DisplayResponse($data);
         $I->checkForData($data);
         $I->wantTo('check if the data corresponds');
         $I->checkPropertyData($data);
-        $I->seeResponseContainsJson(['total' => 415, 'pageTotal'=>21, 'size'=>20]);
         $I->seeResponseIsJson(); 
         $I->seeResponseCodeIs(\Codeception\Util\HttpCode::OK); 
     }
