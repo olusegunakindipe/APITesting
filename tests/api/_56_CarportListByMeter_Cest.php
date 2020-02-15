2cf5dcbb5780a46ded1b383d148226d990ce4f99
aisolae <aremu_abisoye@live.com>
2020-02-16 06:04:33 +0800

    Split api path, Check Json data

--- tests/api/_56_CarportListByMeter_Cest.php
+++ tests/api/_56_CarportListByMeter_Cest.php
@@ -15,18 +15,19 @@ class _56_CarportListByMeter_Cest
     {
         $I->AdminLogin();
         $I->wantTo('check if data is in the API');
-        $data = $I->sendGET('/Carport/ListByMeter/1/20');
+        $page = 1;
+        $size = 20;
+        $urlParams = [
+            $page,
+            $size
+        ];
+        $api = "/Carport/ListByMeter/";
+        $path = $api . join("/", $urlParams);
+        $data = $I->sendGET($path);
         $I->seeResponseCodeIs(\Codeception\Util\HttpCode::OK);
         $I->wantTo('Check response Time for this Api');
         $I->DisplayResponse($data);
         $I->wantTo('See if the data are correct');
-        $I->seeResponseContainsJson([
-            'ELECTRICITY_METER_SN' => '8888',
-            'CELL_ID' => '4503050011',
-            'CELL_NAME' => '七星苑小区',
-            'PILE_NUM' => 1,
-            'DEVICE_STATE' => 1
-        ]);
         $I->CheckForValue($data);
         $I->grabResponse();
         $I->CheckResponseTimeEquals($data);
