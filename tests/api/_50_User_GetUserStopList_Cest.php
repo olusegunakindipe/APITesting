dcc8887a5e20fb9471754874fd5e6f75b5cfd658
aisolae <aremu_abisoye@live.com>
2020-02-16 05:34:00 +0800

    Split api path, Check Json data correct

--- tests/api/_50_User_GetUserStopList_Cest.php
+++ tests/api/_50_User_GetUserStopList_Cest.php
@@ -13,19 +13,20 @@ class _50_User_GetUserStopList_Cest
     public function UserGetUserStopPackage(ApiTester $I)
     {
         $I->AdminLogin();
-        $I->wantTo('check possible data in the API record corresponding');
-        $data=$I->sendGET('/User/GetUserStopList/100000049408/1/20');
+        $I->wantTo('check possible data returns json');
+        $page = 1;
+        $size = 20;
+        $id = '100000049408';
+        $urlParams = [
+            $page,
+            $size,
+            $id
+        ];
+        $api = "/User/GetUserStopList/";
+        $path = $api . join("/", $urlParams);
+        $data = $I->sendGET($path);
         $I->seeResponseCodeIs(\Codeception\Util\HttpCode::OK);
         $I->DisplayResponse($data);
-        $I->dontSeeResponseCodeIs(401);
-        $I->seeResponseIsJson();
-        $I->SeeResponseContainsJson([
-            'IN_USER_ID' => '100000049408',
-            'MOBILE_PHONE' => '13645184146',
-            'AREA' => '江苏省南京市六合区',
-            'USER_ID' => '100000049408',
-            'CREATE_TIME' => '2019-10-22 16:21:13'
-        ]);
         $I->dontSeeResponseCodeIs(401);
         $I->seeResponseIsJson();
         $I->seeResponseCodeIs(200);
