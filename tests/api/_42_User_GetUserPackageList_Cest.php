f362ee53a39c2bb57a27389edf478c03fd5a97d5
aisolae <aremu_abisoye@live.com>
2020-02-16 04:40:30 +0800

    Split Api path, Check if data returned Json

--- tests/api/_42_User_GetUserPackageList_Cest.php
+++ tests/api/_42_User_GetUserPackageList_Cest.php
@@ -16,16 +16,15 @@ class _42_User_GetUserPackageList_Cest
         $I->AdminLogin();
         $I->wantTo('check possible data in the API record corresponding');
         $data=$I->sendGET('/User/GetUserPackageList/100000227661/1/20');
-        $I->wantTo('check if the data is empty');
-        $I->CheckData($data);
-        // $I->seeResponseContainsJson(['total' => 0, 'pageTotal'=>0]);
+        $I->wantTo('check json data');
+        $I->CheckDataFlatComplex($data);
         // $I->wantTo('Get response is empty');
         // $I->CheckForEmptiness($data);
-        // $I->wantTo('Get response Time for this Api');
-        // $I->DisplayResponse($data);
-        // $I->grabResponse();
-        // $I->CheckResponseTimeEquals($data);
-        // $I->seeResponseIsJson();
-        // $I->seeResponseCodeIs(200);
+        $I->wantTo('Get response Time for this Api');
+        $I->DisplayResponse($data);
+        $I->grabResponse();
+        $I->CheckResponseTimeEquals($data);
+        $I->seeResponseIsJson();
+        $I->seeResponseCodeIs(200);
     }
 }
