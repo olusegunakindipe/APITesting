c36cb0c1d66b378f36911e59477e4de831cf3c9f
aisolae <aremu_abisoye@live.com>
2020-02-16 05:25:17 +0800

    Split api path, Check Json data correct

--- tests/api/_48_User_GetUserStopPackage_Cest.php
+++ tests/api/_48_User_GetUserStopPackage_Cest.php
@@ -15,13 +15,20 @@ class _48_User_GetUserStopPackage_Cest
     {
         $I->AdminLogin();
         $I->wantTo('check possible data in the API record corresponding');
-        $data=$I->sendGET('/User/GetUserStopPackage/100000049408/1/20');
+        $page = 1;
+        $size = 20;
+        $id = '100000049408';
+        $urlParams = [
+            $page,
+            $size,
+            $id
+        ];
+        $api = "/User/GetUserStopPackage/";
+        $path = $api . join("/", $urlParams);
+        $data = $I->sendGET($path);
         $I->seeResponseCodeIs(\Codeception\Util\HttpCode::OK);
         $I->DisplayResponse($data);
         $I->dontSeeResponseCodeIs(401);
-        $I->seeResponseIsJson();
-        $I->SeeResponseContainsJson(['data' => []]);
-        $I->SeeResponseContainsJson(['code' => 200]);
         $I->seeResponseIsJson();
         $I->seeResponseCodeIs(\Codeception\Util\HttpCode::OK); 
     }
