6c86fea68881f64bc3084b086ad8ec9b19d82ba3
aisolae <aremu_abisoye@live.com>
2020-02-16 01:20:47 +0800

    Splitted api, Checked order list correction

--- tests/api/_15_Order_ListBy_Cest.php
+++ tests/api/_15_Order_ListBy_Cest.php
@@ -14,18 +14,22 @@ class _15_Order_ListBy_Cest
     {
         $I->AdminLogin();
         $I->wantTo('check if data is accurate');
-        $data = $I->sendGET('Order/ListBy/top/2019-02-07/2020-02-07/1/20');
+        $startDate = "2020-01-07";
+        $endDate = "2020-02-07";
+        $page = 1;
+        $size = 20;
+        $urlParams = [
+            $startDate,
+            $endDate,
+            $page,
+            $size
+        ];
+        $api = "Order/ListBy/top/";
+        $path = $api . join("/", $urlParams);
+        $data = $I->sendGET($path);
         $I->wantTo('Check response Time for this Api');
-        $I->TestData($data);
-        $I->seeResponseContainsJson([ 
-            'CHARGE_ORDER_ID' => 'CO20190409011533143cb6d874f4bc49',
-            'USER_ID' => '100000071899',
-            'CELL_ID' => '4413020005',
-            'CREATE_TIME' => '2019-04-09 01:15:33',
-            'END_INFO' => '充电时间到期'
-            ]
-        );
-       
+        $I->checkOrderListData($data);
+        $I->dontSeeResponseCodeIs(401);
         $I->seeResponseIsJson();
         $I->seeResponseCodeIs(200);
     }
