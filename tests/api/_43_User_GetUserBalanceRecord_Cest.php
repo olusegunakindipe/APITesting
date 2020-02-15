3de71908f658c2308f53ea3daa97c7db756b1ab5
aisolae <aremu_abisoye@live.com>
2020-02-16 04:50:29 +0800

    Split Api path, Check if data returned Json

--- tests/api/_43_User_GetUserBalanceRecord_Cest.php
+++ tests/api/_43_User_GetUserBalanceRecord_Cest.php
@@ -15,19 +15,21 @@ class _43_User_GetUserBalanceRecord_Cest
     {
         $I->AdminLogin();
         $I->wantTo('check possible data in the API record corresponding');
-        $data=$I->sendGET('/User/GetUserBalanceRecord/100000227661/1/20');
+        $page = 1;
+        $size = 20;
+        $id = '100000227661';
+        $urlParams = [
+            $page,
+            $size,
+            $id
+        ];
+        $api = "/User/GetUserBalanceRecord/";
+        $path = $api . join("/", $urlParams);
+        $data = $I->sendGET($path);
         $I->wantTo('Get response Time for this Api');
         $I->DisplayResponse($data);
-        $I->wantTo('check if the data is empty');
-        $I->seeResponseContainsJson(['total' => 1, 'pageTotal'=>1, 'size'=>20]);
-     
-        $I->SeeResponseContainsJson(['data' => [
-            'ORDER_ID' => 'CO20200211170757bb4546f9ba945068',
-            'PAY_STATE' => 1,
-            'moneyType' => 1,
-            'createTime' => '2020-02-11 21:08:25'
-        ]]);
-        $I->SeeResponseContainsJson(['query' => 2]);
+        $I->wantTo('check if response is in Json');
+        // $I->getUserBalanceData($data);
         $I->seeResponseIsJson();
         $I->seeResponseCodeIs(200);
     }
