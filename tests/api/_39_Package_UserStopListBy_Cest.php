3f242f8f9ff29df63a3743383815d3969ac9eeaf
aisolae <aremu_abisoye@live.com>
2020-02-16 03:57:10 +0800

    Splitted api path, Checked json response data not empty

--- tests/api/_39_Package_UserStopListBy_Cest.php
+++ tests/api/_39_Package_UserStopListBy_Cest.php
@@ -14,18 +14,22 @@ class _39_Package_UserStopListBy_Cest
     {
         $I->AdminLogin();
         $I->wantTo('check possible data in the API record corresponding');
-        $data=$I->sendGET('Package/UserStopListBy/2019-02-07/2020-02-07/1/20');
+        $startDate = "2020-01-07";
+        $endDate = "2020-02-07";
+        $page = 1;
+        $size = 20;
+        $urlParams = [
+            $startDate,
+            $endDate,
+            $page,
+            $size
+        ];
+        $api = "/Package/UserStopListBy/";
+        $path = $api . join("/", $urlParams);
+        $data = $I->sendGET($path);
         $I->DisplayResponse($data);
-        $I->wantTo('check if the data is empty');
-        $I->wantTo('Check for Data and also the page numbers');
         $I->CheckResponseTimeEquals($data);
-        $I->SeeResponseContainsJson(['total'=>1195, 'pageTotal'=>60, 'size' => 20]);
-        $I->SeeResponseContainsJson(['data' => [
-            'PACKAGE_ORDER_ID' => 'YANCP202002030158212116465771',
-            'USER_ID' => '100000211181',
-            'NICK_NAME' => 'summer',
-            'BILLING_TYPE' => 1
-        ]]);
+        $I->CheckCarportChargeData($data);
         $I->dontSeeResponseCodeIs(401);
         $I->seeResponseIsJson();
         $I->seeResponseCodeIs(200);
