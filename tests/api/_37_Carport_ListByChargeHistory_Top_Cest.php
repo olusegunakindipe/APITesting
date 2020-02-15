70ca4093f2315bd275326eeeb23499a834275bcd
aisolae <aremu_abisoye@live.com>
2020-02-16 03:42:04 +0800

    Splitted api path, Checked json data correct

--- tests/api/_37_Carport_ListByChargeHistory_Top_Cest.php
+++ tests/api/_37_Carport_ListByChargeHistory_Top_Cest.php
@@ -15,12 +15,22 @@ class _37_Carport_ListByChargeHistory_Top_Cest
     {
         $I->AdminLogin();
         $I->wantTo('check possible data in the API record corresponding');
+        $startDate = "2020-01-07";
+        $endDate = "2020-02-07";
+        $page = 1;
+        $size = 20;
+        $urlParams = [
+            $page,
+            $size
+        ];
+        $api = "/Carport/ListByChargeHistory/top/";
+        $path = $api . join("/", $urlParams);
+        $data = $I->sendGET($path);
         $data=$I->sendGET('Carport/ListByChargeHistory/top/2019-02-07/2020-02-07/1/20');
         $I->DisplayResponse($data);
         $I->dontSeeResponseCodeIs(401);
         $I->wantTo('Check for Carport Charge History');
         $I->CheckCarportChargeHistory($data);
-        // $I->CheckResponseTimeEquals($data);
         $I->seeResponseIsJson();
         $I->seeResponseCodeIs(200);
     }
