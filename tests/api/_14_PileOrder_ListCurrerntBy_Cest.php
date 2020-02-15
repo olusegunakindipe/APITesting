aec8f47df01520fb6bcceafaf19e7c74ea0a642e
aisolae <aremu_abisoye@live.com>
2020-02-16 01:10:43 +0800

    Splitted api, Checked data presence

--- tests/api/_14_PileOrder_ListCurrerntBy_Cest.php
+++ tests/api/_14_PileOrder_ListCurrerntBy_Cest.php
@@ -15,7 +15,15 @@ class _14_PileOrder_ListCurrerntBy_Cest
     {
         $I->AdminLogin();
         $I->wantTo('Check if the some data are present in the API');
-        $data = $I->sendGET('PileOrder/ListCurrentBy/1/20');
+        $page = 1;
+        $size = 20;
+        $urlParams = [
+            $page,
+            $size
+        ];
+        $api = "/PileOrder/ListCurrentBy/";
+        $path = $api . join("/", $urlParams);
+        $data = $I->sendGET($path);
         $I->grabResponse();
         $I->CheckPresence($data);
         $I->wantTo('Get response Time for this Api');
