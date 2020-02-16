135787788ae71b275a7bebb98ab35491ec740b2b
aisolae <aremu_abisoye@live.com>
2020-02-17 04:03:52 +0800

    Split api path,Check response matches json path, data authorization

--- tests/api/_58_Province_ListCarportAll_Cest.php
+++ tests/api/_58_Province_ListCarportAll_Cest.php
@@ -17,10 +17,14 @@ class _58_Province_ListCarportAll_Cest
         $I->wantTo('Get to the data in the List');
         $data = $I->sendGET('Province/ListByCarportAll');
         $I->wantTo('Check the correctness of these data');
-        $I->GetProvinceList($data);
+        // $I->GetProvinceList($data);
         $I->DisplayResponse($data);
         $I->grabResponse();
-        $I->CheckResponseTimeEquals($data);
+        // $I->CheckResponseTimeEquals($data);
+        $I->dontSeeResponseContainsJson(['code' => 404]);
+        $I->dontSeeResponseContainsJson(['data' => null]);
+        $I->seeResponseJsonMatchesJsonPath('$.data[0].DISTRICT_CODE');
+        $I->seeResponseJsonMatchesJsonPath('$.data[0].DISTRICT_NAME');
         $I->seeResponseIsJson();
         $I->DontSeeResponseCodeIs(401);
         $I->seeResponseCodeIs(\Codeception\Util\HttpCode::OK);
