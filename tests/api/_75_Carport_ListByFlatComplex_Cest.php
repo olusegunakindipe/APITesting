7d7afe058b43a192d2a7984259d3916d4fce2405
aisolae <aremu_abisoye@live.com>
2020-02-16 11:23:24 +0800

    Splitted api path, Checked json response data

--- tests/api/_75_Carport_ListByFlatComplex_Cest.php
+++ tests/api/_75_Carport_ListByFlatComplex_Cest.php
@@ -15,11 +15,19 @@ class _75_Carport_ListByFlatComplex_Cest
     {
         $I->AdminLogin();
         $I->wantTo('Check if the some data are present in the API');
-        $data = $I->sendGET('/Carport/ListByFlatComplex/1/20');
+        $page = 1;
+        $size = 20;
+        $urlParams = [
+            $page,
+            $size
+        ];
+        $api = "/Carport/ListByFlatComplex/";
+        $path = $api . join("/", $urlParams);
+        $data = $I->sendGET($path);
+
         $I->wantTo('Get response Time for this Api');
         $I->DisplayResponse($data);
         $I->CheckTestData($data);
-        $I->seeResponseContainsJson(['total' => 62, 'pageTotal'=>4, 'size'=>20]);
         $I->seeResponseIsJson(); 
         $I->seeResponseCodeIs(\Codeception\Util\HttpCode::OK); 
     }
