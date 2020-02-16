fda94b98fd7ede0c4218ee2820e3867d07d2f741
aisolae <aremu_abisoye@live.com>
2020-02-17 02:44:11 +0800

    Split api path, Response matches path, Check correct page and size

--- tests/api/_37_Carport_ListByChargeHistory_Top_Cest.php
+++ tests/api/_37_Carport_ListByChargeHistory_Top_Cest.php
@@ -15,22 +15,27 @@ class _37_Carport_ListByChargeHistory_Top_Cest
     {
         $I->AdminLogin();
         $I->wantTo('check possible data in the API record corresponding');
-        $startDate = "2020-01-07";
-        $endDate = "2020-02-07";
+        $startDate ='2020-01-07';
+        $endDate = '2020-02-07';
         $page = 1;
         $size = 20;
         $urlParams = [
+            $startDate,
+            $endDate,
             $page,
             $size
         ];
         $api = "/Carport/ListByChargeHistory/top/";
         $path = $api . join("/", $urlParams);
         $data = $I->sendGET($path);
-        $data=$I->sendGET('Carport/ListByChargeHistory/top/2019-02-07/2020-02-07/1/20');
         $I->DisplayResponse($data);
         $I->dontSeeResponseCodeIs(401);
         $I->wantTo('Check for Carport Charge History');
-        $I->CheckCarportChargeHistory($data);
+        $I->seeResponseJsonMatchesJsonPath('$.data.data[0].CHARGE_ORDER_ID');
+        $I->dontSeeResponseContainsJson(['data' => 'invalid page']);
+        $I->dontSeeResponseContainsJson(['data' => 'invalid size']);
+
+        // $I->CheckCarportChargeHistory($data);
         $I->seeResponseIsJson();
         $I->seeResponseCodeIs(200);
     }
