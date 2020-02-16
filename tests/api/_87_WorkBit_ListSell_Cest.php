e7b5f636cff14f554c44572e1169fe9b5ff37686
aisolae <aremu_abisoye@live.com>
2020-02-16 12:46:24 +0800

    Splitted api path, Checked json response data type

--- tests/api/_87_WorkBit_ListSell_Cest.php
+++ tests/api/_87_WorkBit_ListSell_Cest.php
@@ -18,17 +18,14 @@ class _87_WorkBit_ListSell_Cest
         $data = $I->sendGET('/WorkBit/ListByBisSell/1/20');
         $I->wantTo('Get response Time for this Api');
         $I->DisplayResponse($data);
-        $I->dontSeeResponseMatchesJsonType([
-            'data' => [
-                'SELL_NO' => 'string',
-                'BI_ID' => 'integer',
-                'NAME' => 'string|null',
-                'FLOOR_FEE'=> 'string|null',
-                'FRAN_COST' => 'string',
-                'MAINTEN_FEE' => 'float',
+        $I->SeeResponseMatchesJsonType([
+            'debug' => [
+                'time' => 'float',
+                'peak' => 'float',
+                'query' => 'integer',
+                'cache'=> 'integer',
             ]
         ]);
-        $I->seeResponseContainsJson(['total' => 222, 'pageTotal'=>12]);
         $I->dontSeeResponseCodeIs(401); 
         $I->seeResponseIsJson(); 
         $I->seeResponseCodeIs(200); 
