d50d3ee4333da9085bcbd20d4e73596d16bc59e2
aisolae <aremu_abisoye@live.com>
2020-02-16 08:08:53 +0800

    Splitted data, Checked Json data

--- tests/api/_73_FlatComplex_ListBySite_Cest.php
+++ tests/api/_73_FlatComplex_ListBySite_Cest.php
@@ -15,19 +15,18 @@ class _73_FlatComplex_ListBySite_Cest
     {
         $I->AdminLogin();
         $I->wantTo('Check if the some data are present in the API');
-        $data = $I->sendGET('/FlatComplex/ListBySite/1/20');
+        $page = 1;
+        $size = 20;
+        $urlParams = [
+            $page,
+            $size
+        ];
+        $api = "/FlatComplex/ListBySite/";
+        $path = $api . join("/", $urlParams);
+        $data = $I->sendGET($path);
         $I->wantTo('Get response Time for this Api');
         $I->DisplayResponse($data);
-        $I->wantTo('check if the data is not empty');
-        $I->seeResponseContainsJson(['total' => 731, 'pageTotal'=>37, 'size'=>20]);
-        $I->seeResponseContainsJson([
-            'SITE_NAME'=> '东景苑小区电动车充电站',
-            'CELL_NAME'=>'东景苑',
-            'AREA' => '山西省太原市万柏林区',
-            'CELL_DISTRICT_CODE' => '140109',
-            'LOCATION' => '东景苑测试',
-            'CELL_TYPE' => 1
-        ]);
+        $I->GetFlatComplexSiteList($data);
         $I->seeResponseIsJson();
         $I->seeResponseCodeIs(200); 
     }
