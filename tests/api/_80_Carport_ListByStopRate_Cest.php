a3e765c564ee2cacf7ddc1284edf07d67fd5b6fd
aisolae <aremu_abisoye@live.com>
2020-02-16 11:44:53 +0800

    Splitted api path, Checked json response data

--- tests/api/_80_Carport_ListByStopRate_Cest.php
+++ tests/api/_80_Carport_ListByStopRate_Cest.php
@@ -15,20 +15,19 @@ class _80_Carport_ListByStopRate_Cest
     {
         $I->AdminLogin();
         $I->wantTo('Check if the some data are present in the API');
-        $data = $I->sendGET('/Carport/ListByStopRate/1/20');
+        $page = 1;
+        $size = 20;
+        $urlParams = [
+            $page,
+            $size
+        ];
+        $api = "/Carport/ListByStopRate/";
+        $path = $api . join("/", $urlParams);
+        $data = $I->sendGET($path);
         $I->wantTo('Get response Time for this Api');
         $I->DisplayResponse($data);
+        $I->GetCarportListData($data);
         $I->dontSeeResponseCodeIs(401);
-        $I->seeResponseContainsJson([
-            'RATE_NAME'=> '临停费率',
-            'BILLING_TYPE'=> 2,
-            'CELL_NAME' => '七星苑小区',
-            'CARPORT_ID' => '450305001120200106030301',
-            'DAY' => 1,
-            'CELL_ID' => '4503050011',
-            'UPDATE_TIME'=> '2020-01-13 17:32:10.480'
-        ]);
-        $I->seeResponseContainsJson(['total' => 43, 'pageTotal'=>3]);
         $I->seeResponseIsJson(); 
         $I->seeResponseCodeIs(\Codeception\Util\HttpCode::OK); 
     }
