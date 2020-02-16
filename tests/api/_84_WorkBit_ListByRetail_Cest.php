7cb4a4365c0fc07e53b62b14bf5e9c85557dd790
aisolae <aremu_abisoye@live.com>
2020-02-16 12:24:25 +0800

    Splitted api path, Checked json response data

--- tests/api/_84_WorkBit_ListByRetail_Cest.php
+++ tests/api/_84_WorkBit_ListByRetail_Cest.php
@@ -15,20 +15,20 @@ class _84_WorkBit_ListByRetail_Cest
     {
         $I->AdminLogin();
         $I->wantTo('Check if the some data are present in the API');
-        $data = $I->sendGET('/WorkBit/ListByBisRetail/1/20');
+        $page = 1;
+        $size = 20;
+        $urlParams = [
+            $page,
+            $size
+        ];
+        $api = "/WorkBit/ListByBisRetail/";
+        $path = $api . join("/", $urlParams);
+        $data = $I->sendGET($path);
         $I->wantTo('Get response Time for this Api');
         $I->DisplayResponse($data);
         $I->grabResponse();
         $I->dontSeeResponseCodeIs(401);
-        $I->seeResponseContainsJson([
-            'BI_ID'=> '1',
-            'SPLIT_TYPE' => '1',
-            'RATIO_CHARGE_FEE' => '10.0000',
-            'RETAIL_BEAR' => 0,
-            'RETAIL_TYPE_NO' => 'RA166578220698731520',
-            'CREATE_TIME'=> '2020-01-13 16:09:30'
-        ]);
-        $I->seeResponseContainsJson(['total' => 423, 'pageTotal'=>22]);
+        $I->TestWorkBitRetailData($data);
         $I->seeResponseIsJson(); 
         $I->seeResponseCodeIs(200); 
         $I->seeResponseCodeIs(\Codeception\Util\HttpCode::OK); 
